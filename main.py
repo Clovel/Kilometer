b73340b050080ef29d4050c2758092c5c576b9dd
Clovis Durand <cd.clovel19@gmail.com>
2019-10-04 14:26:09 +0200

    Updated main.py to use the parser, sorter & plotter

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+import OdometryCSVParser
+import OdometryEntrySorter
+import Plotter
 
 
 def main():
     print("Hello world, this is Kilometer, the Kilometer parser")
+
+    entries = []
+    file = "odometry-data.csv"
+
+    # Get entries from the CSV file
+    OdometryCSVParser.parse_csv(file, entries)
+
+    # Print those entries
+    print("[INFO ] Got the following entries from the CSV file")
+    for entry in entries:
+        entry.print()
+
+    OdometryEntrySorter.sort_odometry_entries(entries)
+
+    # Print the sorted entries
+    print()  # Newline
+    for entry in entries:
+        entry.print()
+
+    Plotter.plot_odometry(entries)
+    Plotter.plot_cost(entries)
+
+    Plotter.show()
 
     return 0
 
